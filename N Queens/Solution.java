72136f089b67c8177ce800a3c6dbdfe50914ca2f
Arivoli AE <aayakarambla@vmware.com>
2019-08-07 18:38:11 -0700

    Commit for Backtracking 3 - N Queens

---
+++ N Queens/Solution.java
@@ -0,0 +1,86 @@
+import java.util.*;
+
+class NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        result.add(new ArrayList<String>());
+        for(int i=0;i<n;i++){
+            //List<String> row = new ArrayList<>();
+            String row = "";
+            for(int j=0;j<n;j++){
+                row += ".";
+            }
+            
+            result.get(0).add(row);
+        }
+
+        solveNQueensHandler(result, n, 0);
+        return result;
+    }
+
+    public boolean isSafe(List<List<String>> result, int row, int col){
+
+        // left side of current row
+
+        for(int i=0;i<col;i++){
+            if(result.get(0).get(row).charAt(i) == 'Q'){
+                return false;
+            }
+        }
+
+        // upper diagonal
+
+        for(int i=row-1, j=col-1; i>=0&&j>=0; i--, j--){
+            if(result.get(0).get(i).charAt(j) == 'Q'){
+                return false;
+            }
+        }
+
+        //lower diagonal
+        for(int i=row+1, j=col-1; i<result.size()&&j>=0; i++, j--){
+            if(result.get(0).get(i).charAt(j) == 'Q'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean solveNQueensHandler(List<List<String>> result, int n, int col){
+
+        //base case
+        if(col == n){
+            return true;
+        }
+        for(int i=0;i<result.size();i++){
+
+            //check if safe
+            if(isSafe(result, i, col) == true){
+                String temp = result.get(0).get(i); 
+                temp.toCharArray()[col] = 'Q';
+                //result.get(i).add(col, "Q");
+                result.get(0).add(i, temp);
+                if(solveNQueensHandler(result, n, col+1) == true){
+                    return true;
+                }
+                String tempBacktrack = result.get(0).get(i); 
+                tempBacktrack.toCharArray()[col] = '.';
+                result.get(0).add(i, tempBacktrack);
+
+                // result.get(i).remove(col);
+                // result.get(i).add(col, ".");
+
+            }
+        }
+
+        return false;
+    }
+}
+
+class Solution {
+    public static void main(String[] args){
+        System.out.println("NQueens");
+        NQueens obj = new NQueens();
+        System.out.println(obj.solveNQueens(4));
+    }
+}
