4fd92a1c084aee52820cd0a6ca991d5f6241c47d
keerthitejakonuri <keerthi49100@gmail.com>
2019-11-13 21:37:13 -0600

    dirs array
    dirs array

--- WordExists.java
+++ WordExists.java
@@ -6,30 +6,36 @@
 
 //Your code here along with comments explaining your approach
 class WordExists {
-    public boolean exist(char[][] board, String word) {
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0)){
-                    if(dfs(board,i,j,0,word)){return true;}
-                }
-            }
-        }
-        return false;
-    }
-    private boolean dfs(char[][] board, int i, int j,int index, String word){
-        if(i >= 0 && j >= 0 && i < board.length && j < board[0].length && board[i][j] != '`'){
-            char ch = board[i][j];
-            if(ch == word.charAt(index)){
-                if(index == word.length()-1){
-                    return true;
-                }
-                board[i][j] = '`';
-                if(dfs(board,i+1,j,index+1,word) || dfs(board,i-1,j,index+1,word) || dfs(board,i,j-1,index+1,word) || dfs(board,i,j+1,index+1,word)){
-                    return true;
-                }
-                board[i][j] = ch;
-            }
-        }
-        return false;
-    }
+	  public boolean exist(char[][] board, String word) {
+	        if(null == word || word.length() == 0 || null == board || board.length == 0){return false;}
+	        int dirs[][] = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+	        for(int i = 0; i < board.length; i++){
+	            for(int j = 0; j < board[0].length; j++){
+	                if(board[i][j] == word.charAt(0)){
+	                    if(dfs(board,i,j,0,word, dirs)){return true;}
+	                }
+	            }
+	        }
+	        return false;
+	    }
+	    private boolean dfs(char[][] board, int i, int j,int index, String word, int[][] dirs){
+	        if(i >= 0 && j >= 0 && i < board.length && j < board[0].length && board[i][j] != '`'){
+	            char ch = board[i][j];
+	            if(ch == word.charAt(index)){
+	                if(index == word.length()-1){
+	                    return true;
+	                }
+	                board[i][j] = '`';
+	                for(int dir[]: dirs){
+	                  int newI = i + dir[0];
+	                  int newJ = j + dir[1];
+	                    if(dfs(board,newI,newJ,index+1,word,dirs)){
+	                        return true;
+	                    }
+	                }
+	                board[i][j] = ch;
+	            }
+	        }
+	        return false;
+	    }
 }
