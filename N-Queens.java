2f7ec0c51c0ec6d6f782a3d70846fd2341ae8ca0
Pavani <pavani.somarouthu@sjsu.edu>
2020-03-11 17:19:26 -0700

    BT3

---
+++ N-Queens.java
@@ -0,0 +1,18 @@
+// Time Complexity : 
+// Space Complexity : 
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
