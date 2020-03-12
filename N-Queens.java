1513f5f3320086f2a488d3cf0874bce60d7f2845
Pavani <pavani.somarouthu@sjsu.edu>
2020-03-12 16:21:59 -0700

    BT3

--- N-Queens.java
+++ N-Queens.java
@@ -1,5 +1,5 @@
-// Time Complexity : 
-// Space Complexity : 
+// Time Complexity : O(N!)
+// Space Complexity : o(N)
 // Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :
 
@@ -16,3 +16,60 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+         List<List<Integer>> result =new LinkedList<>();
+        List<Integer> state= new ArrayList<>();
+        backtrack(result,state,n,0);
+        System.out.println(result);
+        return null;
+    }
+    private void backtrack(List<List<Integer>> result, List<Integer> state,int n,int row){
+        if(row == n){
+            result.add(new LinkedList<>(state));
+            return;
+        }
+        for(int i=0;i<n;i++){
+            if(issafetoPlace(state,n,row,i)){
+                state.add(i);
+                backtrack(result,state,n,row+1);
+                state.remove(state.size()-1);
+            }
+        }
+    }
+    private boolean issafetoPlace(List<Integer> state,int n,int row,int col){
+       return diagonalcheck(state,n,row,col) &&  verticalcheck(state,n,row,col) ;
+    }
+    private boolean verticalcheck(List<Integer> state,int n,int row,int col){
+       int i=row-1;
+        while(i>=0){
+            if(state.get(i)==col){return false;}
+            i--;
+        }
+        return true;
+    }
+    private boolean diagonalcheck(List<Integer> state,int n,int row,int col){
+        int i=row-1;
+        int j=col-1;
+        while(i>=0 && j>=0){
+            if(state.get(i)==j){
+                return false;
+            }
+        i--;
+        j--;
+        }
+        
+         int p=row-1;
+        int q=col+1;
+        while(p>=0 && q<n){
+            if(state.get(p)==q){
+                return false;
+            }
+        p--;
+        q++;
+        }
+        return true;
+    }
+}
--- Word Search.java
+++ Word Search.java
@@ -1,6 +1,6 @@
-// Time Complexity : 
-// Space Complexity : 
-// Did this code successfully run on Leetcode :NO
+// Time Complexity : (m*n)^2
+// Space Complexity :  m*n
+// Did this code successfully run on Leetcode :Yes
 // Any problem you faced while coding this :
 
 
@@ -17,43 +17,45 @@
  * }
  */
 class Solution {
-  public boolean exist(char[][] board, String word) {
-      int m = board.length;
-      int n = board[0].length;
-   
-      boolean result = false;
-      for(int i=0; i<m; i++){
-          for(int j=0; j<n; j++){
-             if(dfs(board,word,i,j,0)){
-                 result = true;
-             }
-          }
-      }
-   
-      return result;
-  }
-   
-  public boolean dfs(char[][] board, String word, int i, int j, int k){
-      int m = board.length;
-      int n = board[0].length;
-   
-      if(i<0 || j<0 || i>=m || j>=n){
-          return false;
-      }
-   
-      if(board[i][j] == word.charAt(k)){
-          char temp = board[i][j];
-          if(k==word.length()-1){
-              return true;
-          }else if(dfs(board, word, i+1, j, k+1)
-  
-          ||dfs(board, word, i, j+1, k+1)){
-              return true;
-          }
-          board[i][j]=temp;
-      }
-   
-      return false;
-  }
-  }
-  
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+     
+        boolean result = false;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+               if(dfs(board,word,i,j,0)){
+                   result = true;
+               }
+            }
+        }
+     
+        return result;
+    }
+     
+    public boolean dfs(char[][] board, String word, int i, int j, int k){
+        int m = board.length;
+        int n = board[0].length;
+     
+        if(i<0 || j<0 || i>=m || j>=n){
+            return false;
+        }
+     
+        if(board[i][j] == word.charAt(k)){
+            char temp = board[i][j];
+            board[i][j]='#';
+            if(k==word.length()-1){
+                return true;
+            }else if(dfs(board, word, i-1, j, k+1)
+            ||dfs(board, word, i+1, j, k+1)
+            ||dfs(board, word, i, j-1, k+1)
+            ||dfs(board, word, i, j+1, k+1)){
+                return true;
+            }
+            board[i][j]=temp;
+        }
+     
+        return false;
+    }
+    }
+    
