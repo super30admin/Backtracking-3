56eb4d73d0d6cc62f6448975a34f892347e9f4f6
Ken Ore <v-kore@expediagroup.com>
2020-09-24 19:11:59 -0500

    problem2 passing leetcode

--- problem2-word-search.coffee
+++ problem2-word-search.coffee
@@ -1,25 +1,24 @@
 #https://leetcode.com/problems/word-search/
-#// Time Complexity :
-#// Space Complexity :
-#// Did this code successfully run on Leetcode :
+#// Time Complexity : O(N^N)
+#// Space Complexity : O(N^N) stack space
+#// Did this code successfully run on Leetcode : yes
 #// Any problem you faced while coding this :
 #
-# internalizing backtracking problems
+# off by 1 error in the base case, comparing to length vs length - 1
 #
 #// Your code here along with comments explaining your approach
+#
+# for each position on the board
+#   find the current character
+#   if you find it, recurse looking for the next character (look up, down, left, right of current position)
+#   if you've found all characters in the input, return true immediately
 
 exist = (board, word) ->
   directions = [[0,1], [0, -1], [1, 0], [-1, 0]]
 
-  exist = () ->
-    for i in [0...board.length]
-      for j in [0...board[0].length]
-        if board[i][j] is word[0]
-          return true if backtrack(i, j, 0)
-
   backtrack = (i, j, index) ->
     # base case
-    if index >= word.length
+    if index >= word.length - 1
       return true
 
     temp = board[i][j]
@@ -37,6 +36,12 @@ exist = (board, word) ->
 
     return false
 
+  for i in [0...board.length]
+    for j in [0...board[0].length]
+      if board[i][j] is word[0]
+        return true if backtrack(i, j, 0)
 
+  false
 
-
+exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED")
+exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"SEE")
--- problem2-word-search.js
+++ problem2-word-search.js
@@ -1,33 +1,26 @@
 //https://leetcode.com/problems/word-search/
-//// Time Complexity :
-//// Space Complexity :
-//// Did this code successfully run on Leetcode :
+//// Time Complexity : O(N^N)
+//// Space Complexity : O(N^N) stack space
+//// Did this code successfully run on Leetcode : yes
 //// Any problem you faced while coding this :
 
-// internalizing backtracking problems
+// off by 1 error in the base case, comparing to length vs length - 1
 
 //// Your code here along with comments explaining your approach
+
+// for each position on the board
+//   find the current character
+//   if you find it, recurse looking for the next character (look up, down, left, right of current position)
+//   if you've found all characters in the input, return true immediately
 var exist;
 
 exist = function(board, word) {
-  var backtrack, directions;
+  var backtrack, directions, i, j, k, l, ref, ref1;
   directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-  exist = function() {
-    var i, j, k, l, ref, ref1;
-    for (i = k = 0, ref = board.length; (0 <= ref ? k < ref : k > ref); i = 0 <= ref ? ++k : --k) {
-      for (j = l = 0, ref1 = board[0].length; (0 <= ref1 ? l < ref1 : l > ref1); j = 0 <= ref1 ? ++l : --l) {
-        if (board[i][j] === word[0]) {
-          if (backtrack(i, j, 0)) {
-            return true;
-          }
-        }
-      }
-    }
-  };
-  return backtrack = function(i, j, index) {
+  backtrack = function(i, j, index) {
     var c, k, len, r, temp, x, y;
     // base case
-    if (index >= word.length) {
+    if (index >= word.length - 1) {
       return true;
     }
     temp = board[i][j];
@@ -46,6 +39,20 @@ exist = function(board, word) {
     board[i][j] = temp;
     return false;
   };
+  for (i = k = 0, ref = board.length; (0 <= ref ? k < ref : k > ref); i = 0 <= ref ? ++k : --k) {
+    for (j = l = 0, ref1 = board[0].length; (0 <= ref1 ? l < ref1 : l > ref1); j = 0 <= ref1 ? ++l : --l) {
+      if (board[i][j] === word[0]) {
+        if (backtrack(i, j, 0)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
+
+exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED");
+
+exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "SEE");
 
 //# sourceMappingURL=problem2-word-search.js.map
