9c25c26e729a20f4463e7cde7ff9d3852bef0891
Owais Kazi <owais@Owaiss-MacBook-Pro.local>
2020-09-24 18:22:27 -0700

    Updates N-queens

--- N-queens-51.py
+++ N-queens-51.py
@@ -2,58 +2,62 @@
 
 # Time Complexity : O(n*m!)
 # Space Complexity : O(n)  
-# Did this code successfully run on Leetcode : No (There's some problem with LC. The below code is running well when I am having n = 1 but giving an error while submitting)
+# Did this code successfully run on Leetcode : Yes
 # Any problem you faced while coding this : No
 
 class Solution(object):
     
-    right_diagonal = set()
-    left_diagonal = set()
-    col = set()
-    output = []
-    
-    def isValid(self, board, c, r):
-        #check whether positon is alredy occupied
-        if c not in self.col and r - c not in self.left_diagonal and r + c not in self.right_diagonal:
-            return True
-        return False
-    
-    def backtrack(self, board, queensLeft, row):
-        if queensLeft == 0:
-            self.output.append(self.makeBoard(board))
-            
-        for c in range(len(board)):
-            #check for valid position
-            if self.isValid(board, c, row):
-                #backtracking
-                board[row][c] = "Q"
-                self.right_diagonal.add(row+c)
-                self.left_diagonal.add(row-c)
-                self.col.add(c)
-                
-                self.backtrack(board, queensLeft - 1, row + 1)
-                
-                board[row][c] = "."
-                self.right_diagonal.remove(row+c)
-                self.left_diagonal.remove(row-c)
-                self.col.remove(c)
-    
-    def makeBoard(self, board):
-        res = []
-        for i in range(len(board)):
-            temp = ''
-            for j in range(len(board[0])):
-                temp += board[i][j]
-            res.append(temp)
-        return res
-        
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
         """
+        
+        def isValid(board, c, r):
+            #check whether positon is alredy occupied
+            if c not in col and r - c not in left_diagonal and r + c not in right_diagonal:
+                return True
+            return False
+    
+        def backtrack(board, queensLeft, row):
+            if queensLeft == 0:
+                output.append(makeBoard(board))
+
+            for c in range(len(board)):
+                #check for valid position
+                if isValid(board, c, row):
+                    #backtracking
+                    board[row][c] = "Q"
+                    right_diagonal.add(row+c)
+                    left_diagonal.add(row-c)
+                    col.add(c)
+
+                    backtrack(board, queensLeft - 1, row + 1)
+
+                    board[row][c] = "."
+                    right_diagonal.remove(row+c)
+                    left_diagonal.remove(row-c)
+                    col.remove(c)
+    
+        def makeBoard(board):
+            res = []
+            for i in range(len(board)):
+                temp = ''
+                for j in range(len(board[0])):
+                    temp += board[i][j]
+                res.append(temp)
+            return res
+    
+        #driver code
+        if n == 1:
+            return [["Q"]]
         board = [['.' for _ in range(n)] for _ in range(n)]
         
-        self.backtrack(board, n, 0)
+        right_diagonal = set()
+        left_diagonal = set()
+        col = set()
+        output = []
         
-        return self.output
+        backtrack(board, n, 0)
+        
+        return output
