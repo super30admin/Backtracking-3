98bed2377fb1389fd7eb49162af0eee56cc7fcc9
Adhish Shrivastava <adhish70@gmail.com>
2021-11-15 12:59:15 -0500

    Initial Commit

---
+++ Problem-74 N-Queens.py
@@ -0,0 +1,11 @@
+# 51. N-Queens
+# https://leetcode.com/problems/n-queens/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        pass
---
+++ Problem-75 Word Search.py
@@ -0,0 +1,11 @@
+# 79. Word Search
+# https://leetcode.com/problems/word-search/
+
+# Logic: 
+
+# Time Complexiety: 
+# Space Complexiety: 
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        pass
