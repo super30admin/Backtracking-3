9ab0a01aea377fe0b05a67ebd39b4320803c91d4
plokaiah <plokaiah@asu.edu>
2022-07-07 22:57:46 -0700

    added code by harsh

--- WordSearch.py
+++ WordSearch.py
@@ -37,3 +37,41 @@ class Solution(object):
                 if word[0]==board[i][j]:
                     if(backtrack(board,i,j,word,0)): return True
         return False
+
+#code by harsh
+'''
+def exist2(self, board, word):
+    # idea is to make all the dfs aware if the board was found earlier, so they can quit looking.
+    # we can achieve this by either keep it a global variable or send as reference.
+    found = [False]
+    m, n = len(board), len(board[0])
+    dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+    # search for all word[0] to do dfs on
+    for i in range(m):
+        for j in range(n):
+            if found[0] == False and board[i][j] == word[0]:
+                # since we already matched first char of word, mark it as visited and call dfs from second char in
+                # word.
+                # sending fresh copy of visited for every DFS.
+                visited = [[False] * n for i in range(m)]
+                visited[i][j] = True
+                self.dfs2(board, i, j, word, 1, m, n, found, dirs, visited)
+
+    return found[0]
+
+def dfs2(self, board, r, c, word, index, m, n, found, dirs, visited):
+    # base
+    if index == len(word):
+        found[0] = True
+        return
+    if found[0]:
+        return
+    # logic
+    for dir in dirs:
+        nr = r + dir[0]
+        nc = c + dir[1]
+        if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == word[index] and visited[nr][nc] is False:
+            temp = [visited[i][:] for i in range(m)]
+            temp[nr][nc] = True
+            self.dfs2(board, nr, nc, word, index + 1, m, n, found, dirs, temp)
+'''
