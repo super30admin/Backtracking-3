7d97f24e79df0989bb2bd4a2b528046f20c889bd
Rahil Shah <rahilshah@Rahils-MacBook-Air.local>
2022-08-01 15:58:35 -0700

    Completed Backtracking-3

---
+++ N-Queens.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+
+//Time Complexity- similar to n!
+//Space Complexity- O(n*n)
+//Sucessfully ran on leetcode
+
+x
+class Solution {
+ List<List<String>> result;
+ boolean[][] grid;
+ int n;
+ public List<List<String>> solveNQueens(int n) {
+     result= new ArrayList<>();
+     if(n==0) return result;
+     
+     grid= new boolean[n][n];
+     this.n=n;
+     
+     backtrack(0);
+     return result;
+ }
+ 
+ private void backtrack(int row){
+    //base
+     if(row== n){
+         List<String> answer= new ArrayList<>();
+         for(int i=0; i<n; i++){
+             StringBuilder sb= new StringBuilder();
+             for(int j=0; j<n; j++){
+                 if(grid[i][j]== true){
+                     sb.append('Q');
+                 }
+                 else sb.append('.');
+             }
+             answer.add(sb.toString());
+         }
+         result.add(answer);
+         return;
+     }
+     
+     //logic
+     for(int i=0; i< n; i++){
+         if(isSafe(row ,i)){
+             //action
+             grid[row][i]= true;   
+             //recurse
+             backtrack(row + 1);
+             //backtrack
+             grid[row][i]=false;
+         }
+          
+     }
+ }
+ private boolean isSafe(int r, int c){
+     //check the column first
+     for(int i= r-1; i>=0; i--){
+         if(grid[i][c]== true){
+             return false;
+         }
+     }
+     //check left diagonal
+     int i = r, j = c;
+     while(i>=0 && j>=0){
+         if(grid[i][j]== true){
+             return false;
+            
+         }
+         i--;
+         j--;
+     }
+     //check the right diagonal
+     i=r ;
+     j=c;
+     while(i>=0 && j< n){
+         if(grid[i][j]== true){
+             return false;
+            
+         }
+         i--;
+         j++;
+     }
+     return true;
+ }
+ 
+}
---
+++ WordSearch.java
@@ -0,0 +1,53 @@
+//Time Complexity- Exponential O(m*n* 3^L) where L is the length of the word
+//Space Complexity- O(L)
+//Sucessfully ran on leetcode
+
+class Solution {
+ int m,n;
+ int[][] dirs;
+ public boolean exist(char[][] board, String word) {
+     if(board==null || board.length==0) return false;
+     m= board.length;
+     n= board[0].length;
+     dirs= new int[][] {{-1,0}, {1,0}, {0,1}, {0,-1}}; //U D R L
+     
+     
+     for(int i=0; i< m; i++){
+         for(int j=0;j< n; j++){
+              if(backtrack(board, word, i,j, 0)){
+                  return true;
+              }  
+         }
+     }
+     return false;
+ }
+ 
+ private boolean backtrack(char[][] board, String word, int r, int c, int index){
+     
+     //base
+     if(index== word.length()) return true;
+     if(r<0 || c<0 || r==m || c==n || board[r][c]== '#') return false;
+     
+     //logic
+     if(word.charAt(index)== board[r][c]){
+         //action
+         char ch=  board[r][c];
+         board[r][c]='#';
+         
+         for(int[] dir: dirs){
+             int nr= r+ dir[0];
+             int nc= c + dir[1];
+         
+         //recurse
+        if(backtrack(board, word, nr, nc, index+1)){
+            return true;
+        }
+     }
+         //backtrack
+         board[r][c]= ch;
+     }
+     return false;
+ }
+}
+
+}
