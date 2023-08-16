323df23151bb310b913abfccc485e5e0eb974f7d
somonnoy <sb7238@g.rit.edu>
2023-08-15 23:02:34 -0400

    Completed NQueens

---
+++ NQueens.java
@@ -0,0 +1,68 @@
+// Time Complexity : O(n!)
+// Space Complexity : O(n^2)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+import java.util.*;
+
+class NQueens {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        char[][] board = new char[n][n];
+        for (char[] row : board) {
+            Arrays.fill(row, '.');
+        }
+        backtrack(result, board, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<String>> result, char[][] board, int row) {
+        if (row == board.length) {
+            result.add(constructSolution(board));
+            return;
+        }
+
+        for (int col = 0; col < board.length; col++) {
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                backtrack(result, board, row + 1);
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isValid(char[][] board, int row, int col) {
+        int n = board.length;
+
+        // Check if there's a queen in the same column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there's a queen in the upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // Check if there's a queen in the upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private List<String> constructSolution(char[][] board) {
+        List<String> solution = new ArrayList<>();
+        for (char[] row : board) {
+            solution.add(new String(row));
+        }
+        return solution;
+    }
+}
