eac8b2467c62afb1d887b86eba2fc168413af6f1
Gopi Vaka <vakagopi80@gmail.com>
2023-09-12 11:23:03 -0500

    completed Backtracking-3

---
+++ WordSearch.java
@@ -0,0 +1,45 @@
+class Solution {
+    private int[][] dirs;
+    int m;
+    int n;
+    public boolean exist(char[][] board, String word) {
+        if(board== null ||board.length == 0) return false;
+        dirs = new int [][] {{-1,0},{0,-1},{1,0},{0,1}};
+         m = board.length;
+         n = board[0].length;
+         for (int i =0 ; i < m ; i++ ){
+               for (int j =0 ; j < n ; j++){
+       if (backtrack(board, word , i, j , 0)) return true;
+    }
+         }
+         return false;
+    }
+
+private boolean backtrack(char[][] board, String word, int i , int j, int index ){
+    //base 
+
+     if(i < 0 || j <0 || i == m || j== n || board[i][j] =='#') return false;
+     if(index == word.length() ) return true;
+
+    // logic
+
+    if(board[i][j] == word.charAt(index)){
+        //action
+        board[i][j] = '#';
+        //recurse
+        for (int [] dir : dirs){
+           int nr = i + dir[0];
+           int nc = j + dir[1];
+       if( backtrack(board , word , nr , nc , index+1 )) return true;
+        }
+        //backtrack
+        board[i][j] = word.charAt(index);
+
+    }
+    return false;
+}
+
+}
+        
+    
+    
